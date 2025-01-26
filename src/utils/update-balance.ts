69c69f2c7e3785e5de619b2838c96b812616fc33
@pedrodecf <contato.pedrodecf@gmail.com>
2025-01-26 00:58:21 -0300

    removendo erro de saldo insuficiente

--- src/utils/update-balance.ts
+++ src/utils/update-balance.ts
@@ -1,44 +1,40 @@
-import { Prisma, TipoTransacao, Usuario } from "@prisma/client"
-import { SaldoInsuficienteError } from "../errors/saldo-insuficiente.error"
-import { UsuarioRepository } from "../repositories/usuarios/usuario-repository"
+import { Prisma, TipoTransacao, Usuario } from "@prisma/client";
+import { UsuarioRepository } from "../repositories/usuarios/usuario-repository";
 
 interface Props {
-   usuario: Usuario
-   balanceData: { valor: number, tipo: TipoTransacao }
-   tx: Prisma.TransactionClient
-   usuariosRepository: UsuarioRepository
-   isTipoSaida?: boolean
-   saldoOriginalTransacao?: number
+  usuario: Usuario;
+  balanceData: { valor: number; tipo: TipoTransacao };
+  tx: Prisma.TransactionClient;
+  usuariosRepository: UsuarioRepository;
+  isTipoSaida?: boolean;
+  saldoOriginalTransacao?: number;
 }
 
 export async function updateBalance({
-   usuario,
-   balanceData,
-   tx,
-   usuariosRepository,
-   isTipoSaida = false,
-   saldoOriginalTransacao
+  usuario,
+  balanceData,
+  tx,
+  usuariosRepository,
+  // isTipoSaida = false,
+  // saldoOriginalTransacao
 }: Props): Promise<Usuario> {
-   const { valor, tipo } = balanceData
+  const { valor, tipo } = balanceData;
 
-   if (tipo === TipoTransacao.Saida && usuario.balanco < valor && !isTipoSaida) {
-      throw new SaldoInsuficienteError()
-   }
+  // if (tipo === TipoTransacao.Saida && usuario.balanco < valor && !isTipoSaida) {
+  //    throw new SaldoInsuficienteError()
+  // }
 
-   if (isTipoSaida && saldoOriginalTransacao) {
-      const novoBalanco = usuario.balanco + saldoOriginalTransacao
-      if (tipo === TipoTransacao.Saida && novoBalanco < valor) {
-         throw new SaldoInsuficienteError()
-      }
-   }
+  // if (isTipoSaida && saldoOriginalTransacao) {
+  //    const novoBalanco = usuario.balanco + saldoOriginalTransacao
+  //    if (tipo === TipoTransacao.Saida && novoBalanco < valor) {
+  //       throw new SaldoInsuficienteError()
+  //    }
+  // }
 
-   const novoBalanco = tipo === TipoTransacao.Entrada
+  const novoBalanco =
+    tipo === TipoTransacao.Entrada
       ? usuario.balanco + valor
-      : usuario.balanco - valor
+      : usuario.balanco - valor;
 
-   return usuariosRepository.updateBalanco(
-      usuario,
-      novoBalanco,
-      tx
-   )
+  return usuariosRepository.updateBalanco(usuario, novoBalanco, tx);
 }
